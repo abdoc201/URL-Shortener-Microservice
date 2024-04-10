0c13035b4edd49966b4203d8eeef89c806175b68
abdo casa <abdoc201@gmail.com>
2024-04-10 16:19:03 +0000

    modified

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let dataStorage = {}
+
 const numGenerator = (req)=>{
     let x = req.body.url;
     let t = 0;
@@ -26,26 +28,44 @@ const numGenerator = (req)=>{
     return t;
 }
 
+const generateId = () => Math.random().toString(36).substr(2, 9);
+
 app.post('/api/shorturl',bodyParser.urlencoded({extended: false}),(req,res)=>{
   if(/^https?:\/\/www[.].+[.]com.*$/.test(req.body.url)){
     dns.lookup(req.body.url.match(/[.](.+$)/)[1],(error,address,family)=>{
         if(error){
           console.log(error)
           res.json({
-            error: 'invalid url'
+            error: 'this website does not exist'
           })
         }
         else {
           console.log(`The ip address is ${address} and the ip version is ${family}`)
+          const id = numGenerator(req)
+          dataStorage[id] = req.body.url
           res.json({
             original_url: req.body.url ,
-            short_url: numGenerator(req)
+            short_url: id
           })
         }
     });
 }
+else res.json({
+        error: 'invalid url'
+      })
 })
 
+
+app.get('/api/shorturl/:id',(req,res)=>{
+  const data = dataStorage[req.params.id]
+  if(data){
+    console.log("wroking")
+    res.status(301).redirect(`${data}`)
+  }
+  else {
+      res.status(404).json({ error: 'Data not found' });
+  } 
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
